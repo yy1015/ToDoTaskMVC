f6cdee57d81bfa8f821b0ea967506032ff58d4ce
Jerry <jerry.bst@outlook.com>
2018-03-01 20:33:02 +1100

    Add create to do page.

---
+++ ToDoTasksMVC/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoTasksMVC.Models.ViewModels;
+
+namespace ToDoTasksMVC.Controllers
+{
+    public class TodoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AddTaskViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ToDoTasksMVC/Models/ViewModels/AddTaskViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoTasksMVC.Models.ViewModels
+{
+    public class AddTaskViewModel
+    {
+        [Required]
+        [Display( Name = "Task Name" )]
+        public string TaskName { get; set; }
+
+        [Required]
+        [Display(Name = "Task Description123")]
+        public string TaskDescription { get; set; }
+
+        public bool HasReminder { get; set; }
+
+        public DateTime? ReminderDate { get; set; }
+    }
+}
